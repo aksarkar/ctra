efb67a8b8b1fe9c5cad0a69be3a09bf14f50b7d1
Abhishek Sarkar <aksarkar@mit.edu>
2015-11-20 12:22:44 -0500

    Set up packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='pybslmm',
+    description='Python implementation of Bayesian Sparse Linear Mixed Models',
+    url='https://github.mit.edu/aksarkar/pybslmm',
+    author='Abhishek Sarkar',
+    author_email='aksarkar@mit.edu',
+    license='BSD',
+    install_requires=['numpy', 'scipy']
+)
