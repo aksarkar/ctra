809e235a290dfd331bfd4db3c0e2280f9a75caf9
Abhishek Sarkar <aksarkar@mit.edu>
2016-04-21 14:40:20 -0400

    Output binary phenotypes from probit model

--- pybslmm/simulation.py
+++ pybslmm/simulation.py
@@ -3,11 +3,12 @@ import numpy.random as R
 
 def simulate(n, p, pve):
     maf = R.uniform(0.05, 0.5, size=p)
-    x = numpy.array([R.binomial(2, p, size=n) for p in maf])
-    beta = numpy.zeros(p)
+    x = R.binomial(2, maf, size=(n, maf.shape[0])).astype(float)
+    x -= x.mean(axis=0)[numpy.newaxis,:]
+    theta = numpy.zeros(p)
     m = p // 10
-    beta[:m] = R.normal(size=m)
-    g = x.dot(beta)
-    e = R.normal(scale=numpy.var(g) * (1 / pve - 1))
-    y = g + e
-    return x, y, beta
+    theta[:m] = R.normal(size=m)
+    g = numpy.dot(x, theta)
+    e = R.normal(scale=numpy.var(g) * (1 / pve - 1), size=n)
+    y = (g + e > 0).astype(int)
+    return x, y, theta
