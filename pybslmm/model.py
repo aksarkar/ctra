f1e8e59671208615d27059e059e948f9e221f506
Abhishek Sarkar <aksarkar@mit.edu>
2016-04-26 12:37:48 -0400

    Implement Beta regression
    The Beta distribution is technically not appropriate for binary
    responses, but we simply clamp the observed values into its support. We
    might prefer Beta response to a binomial response (logit link) because
    it will handle responses close to the decision boundary (E[y] > 0.5)
    better.

--- pybslmm/model.py
+++ pybslmm/model.py
@@ -16,7 +16,37 @@ _F = theano.function
 _S = theano.shared
 _Z = lambda n: numpy.zeros(n).astype(_real)
 
-def fit(X_, y_, num_epochs=5000, max_precision=1e8, a=1e-3, b1=0.9, b2=0.999, e=1e-8):
+def logit(y, eta):
+    """Return E_q[ln p(y | eta)] assuming a logit link."""
+    return T.mean(T.sum(y * eta - T.nnet.softplus(eta), axis=1))
+
+def beta(y, eta, eps=1e-7):
+    """Return E_q[ln p(y | eta)] assuming y ~ Beta(sigmoid(eta), exp(-eta))
+
+    The Beta distribution is technically not appropriate for binary responses,
+    but we simply clamp the observed values into its support. We might prefer
+    Beta response to a binomial response (logit link) because it will handle
+    responses close to the decision boundary (E[y] > 0.5) better.
+
+    """
+    y = T.clip(y, eps, 1 - eps)
+    m = T.nnet.sigmoid(eta)
+    v = T.exp(-eta)
+    return T.mean(m * v * (T.log(y) - T.log(1 - y)) + v * T.log(1 - y) +
+                  T.gammaln(v) - T.gammaln(m * v) - T.gammaln((1 - m) * v))
+
+def fit(X_, y_, llik=logit, num_epochs=5000, max_precision=1e6, a=1e-3, b1=0.9, b2=0.999, e=1e-8):
+    """Return the variational parameters alpha, beta, gamma.
+
+    X_ - dosage matrix (n x p)
+    y_ - response vector (n x 1)
+    llik - data likelihood under the variational approximation. A function
+           which takes parameters y, mu, nu and returns a TensorVariable
+    num_epochs - number of stochastic gradient steps
+    max_precision - maximum value of gamma
+    a, b1, b2, e - Adam parameters for auto-tuning learning rate
+
+    """
     if X_.shape[0] != y_.shape[0]:
         raise ValueError("dimension mismatch: X {} vs y {}".format(X_.shape, y_.shape))
     n, p = X_.shape
@@ -38,9 +68,10 @@ def fit(X_, y_, num_epochs=5000, max_precision=1e8, a=1e-3, b1=0.9, b2=0.999, e=
     mu = T.dot(X, alpha * beta)
     nu = T.dot(T.sqr(X), alpha / gamma + alpha * (1 - alpha) + T.sqr(beta))
 
-    # Re-parameterization to make expectation differentiable
+    # Re-parameterize to get a differentiable expectation, following Kingma &
+    # Welling, ICLR 2014 (http://arxiv.org/abs/1312.6114)
     random = T.shared_randomstreams.RandomStreams(seed=0)
-    eta_raw = random.normal(size=(10, n))
+    eta_raw = random.normal(size=(10, y.shape[0]))
     eta = mu + T.sqrt(nu) * eta_raw
 
     # Hyperparameters
@@ -51,7 +82,7 @@ def fit(X_, y_, num_epochs=5000, max_precision=1e8, a=1e-3, b1=0.9, b2=0.999, e=
     # Objective function
     elbo = (
         # Data likelihood
-        T.mean(T.sum(y * eta - T.nnet.softplus(eta), axis=1))
+        llik(y, eta) +
         # Prior
         + .5 * T.sum(alpha * (1 + T.log(tau) - T.log(gamma) - tau * (T.sqr(beta) + 1 / gamma)))
         # Variational entropy
@@ -84,7 +115,7 @@ def fit(X_, y_, num_epochs=5000, max_precision=1e8, a=1e-3, b1=0.9, b2=0.999, e=
             beta.get_value(),
             gamma.eval())
 
-def test(a=1e-3):
+def test(llik=logit, a=1e-3, **kwargs):
     from .simulation import simulate_ascertained_probit
     from sklearn.linear_model import LogisticRegression
 
