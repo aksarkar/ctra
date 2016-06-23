1a6c80cc1c6ed6d583bda4a21eac1380fabc45e8
Abhishek Sarkar <aksarkar@mit.edu>
2016-06-23 11:36:12 -0400

    Write the desired model API
    Precursor to major refactor to make it possible to distribute and
    parallelize.
    
    The key stumbling block is to compile the Theano code once and
    distribute that only. Might not be possible to disentangle the Theano
    graph from the data reference though.

--- pybslmm/evaluate.py
+++ pybslmm/evaluate.py
@@ -12,14 +12,9 @@ import pickle
 import sys
 
 import numpy
-import numpy.random
-import scipy.special
-from sklearn.linear_model import LogisticRegression
 
 import pybslmm.pcgc
 import pybslmm.model
-from .simulation import *
-from .model import fit
 import pybslmm.simulation
 
 def prc(y, p):
@@ -32,25 +27,16 @@ def auprc(y, p):
     delta = numpy.diff(points[:,0], axis=0)
     return points[1:,1].dot(delta)
 
-def evaluate(datafile=None, seed=0, pve=0.5, m=100):
-    if datafile is not None:
-        with open(datafile, 'rb') as f:
-            x, y, a, theta = pickle.load(f)
-    else:
-        numpy.random.seed(seed)
-        x, y, theta = sample_ascertained_probit(n=2000, p=10000, K=.01, P=.5,
-                                                pve=pve, m=m, batch_size=10000)
-    x_train, x_test = x[::2], x[1::2]
-    y_train, y_test = y[::2], y[1::2]
-    baseline = auprc(y_test, LogisticRegression(fit_intercept=False).fit(x_train, y_train).predict_proba(x_test)[:,1])
-    result = None
-    pi = 0.1 + numpy.zeros(2)
-    tau = 1 + numpy.zeros(2)
-    for step in fit(x_train, y_train, a, pi, tau):
-        result = step
-        elbo, alpha, beta, gamma = result
+def evaluate_sgvb(n=2000, p=10000, K=.01, P=.5, pve=0.5, seed=0):
+    annotation_params = [(100, 1), (50, 1)]
+    with pybslmm.simulation.simulation(p, pve, annotation_params, seed) as s:
+        x, y = s.sample_case_control(n=n, K=K, P=P)
+        x_train, x_test = x[::2], x[1::2]
+        y_train, y_test = y[::2], y[1::2]
+        a = s.annot
+        alpha, beta, pi, tau = pybslmm.model.fit(x, y, a)
+        baseline = auprc(y_test, x_test.dot(pybslmm.pcgc.logit_regression(x_train, y_train).x))
         comparison = auprc(y_test, scipy.special.expit(x_test.dot(alpha * beta)))
-        print(elbo, baseline, comparison)
         print(baseline, comparison, pi, tau)
 
 def evaluate_pcgc_two_components(n=1000, p=1000, pve=0.5):
