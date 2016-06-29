c6b8e992dd595935d2c7dda8cca3d51ca2e9178a
Abhishek Sarkar <aksarkar@mit.edu>
2016-06-29 01:02:12 -0400

    Refactor model to compile only once
    We need to write this as a class to keep access to the underlying
    Theano variables and the compiled function implementing SGD.
    
    We pickle the model and use DRMAA to run it on the sampled
    hyperparameters.
    
    Ignore cached datasets generated by the tests.

--- pybslmm/model.py
+++ pybslmm/model.py
@@ -25,16 +25,22 @@ Author: Abhishek Sarkar <aksarkar@mit.edu>
 
 """
 import collections
+import drmaa
+import pickle
+import os
+import sys
 
 import numpy
 import numpy.random as R
+import scipy.misc
 import scipy.special
+import scipy.stats
 import theano
 import theano.tensor as T
 
 _real = theano.config.floatX
 _F = theano.function
-_S = theano.shared
+_S = lambda x: theano.shared(x, borrow=True)
 _Z = lambda n: numpy.zeros(n).astype(_real)
 
 def logit(y, eta):
@@ -42,116 +48,166 @@ def logit(y, eta):
     F = y * eta - T.nnet.softplus(eta)
     return T.mean(T.sum(F, axis=1)) - T.mean(F)
 
-def fit(X_, y_, a_, pi_, tau_, llik=logit, initial_params=None,
-        minibatch_n=100, max_precision=1e5, learning_rate=None,
-        b1=0.9, b2=0.999, e=1e-8):
-    """Return the variational parameters alpha, beta, gamma.
+def _grid(x, y=None):
+    if y is None:
+        y = x
+    return numpy.dstack(numpy.meshgrid(x, y)).reshape(-1, 2)
 
-    X_ - dosage matrix (n x p)
-    y_ - response vector (n x 1)
-    a_ - annotation vector (p x 1), values {0, ..., m - 1}
-    llik - data likelihood under the variational approximation
-    initial_params - Initial values of (alpha, beta, gamma, pi, tau)
-    minibatch_n - size of sample minibatches
-    max_precision - maximum value of gamma
-    learning_rate - initial gradient ascent step size (used for Adam)
-    b1 - first moment exponential decay (Adam)
-    b2 - second moment exponential decay (Adam)
-    e - tolerance (Adam)
+def _clip(a, eps=1e-8):
+    return numpy.clip(a, eps, 1 - eps)
 
-    """
-    if X_.shape[0] != y_.shape[0]:
-        raise ValueError("dimension mismatch: X {} vs y {}".format(X_.shape, y_.shape))
-    if X_.shape[1] != a_.shape[0]:
-        raise ValueError("dimension mismatch: X {} vs a {}".format(X_.shape, a_.shape))
-    n, p = X_.shape
-    m = 1 + max(a_)
+class Model:
+    def __init__(self, X_, y_, a_, llik=logit, minibatch_n=100,
+                 max_precision=1e5, learning_rate=None, b1=0.9, b2=0.999,
+                 e=1e-8):
+        """Return the compiled Theano function which takes a gradient step
 
-    # Observed data
-    X = theano.shared(X_.astype(_real))
-    y_obs = theano.shared(y_)
-    y = T.cast(y_obs, 'int32')
-    a = theano.shared(a_.astype('int32'))
+        llik - data likelihood under the variational approximation
+        max_precision - maximum value of gamma
+        learning_rate - initial gradient ascent step size (used for Adam)
+        b1 - first moment exponential decay (Adam)
+        b2 - second moment exponential decay (Adam)
+        e - tolerance (Adam)
 
-    # Randomly initialize over hyperparameter proposals to find optimal
-    # initialization, then re-run with that initialization for each proposal
-    if initial_params is None:
-        alpha_ = R.uniform(size=p).astype(_real)
-        beta_ = R.normal(size=p).astype(_real)
-        gamma_ = R.normal(size=p).astype(_real)
+        """
+        # Observed data
+        X = _S(X_.astype(_real))
+        y = _S(y_.astype(_real))
+        a = _S(a_.astype('int8'))
+        n, p = X_.shape
+        m = 1 + T.max(a_)
 
-    # Variational parameters
-    alpha_raw = _S(alpha_)
-    alpha = T.nnet.sigmoid(alpha_raw)
-    beta = _S(beta_)
-    gamma_raw = _S(gamma_)
-    gamma = max_precision * T.nnet.sigmoid(gamma_raw)
-    params = [alpha_raw, beta, gamma_raw]
+        # Variational parameters
+        alpha_raw = _S(_Z(p))
+        alpha = T.nnet.sigmoid(alpha_raw)
+        beta = _S(_Z(p))
+        gamma_raw = _S(_Z(p))
+        gamma = max_precision * T.nnet.sigmoid(gamma_raw)
+        self.alpha = alpha
+        self.beta = beta
+        params = [alpha_raw, beta, gamma_raw]
 
-    # Hyperparameters
-    pi = _S(pi_.astype(_real))
-    pi_deref = T.basic.choose(a, pi)
-    tau = _S(tau_.astype(_real))
-    tau_deref = T.basic.choose(a, tau)
+        # Hyperparameters
+        pi = T.vector()
+        pi_deref = T.basic.choose(a, pi)
+        tau = T.vector()
+        tau_deref = T.basic.choose(a, tau)
 
-    # We need to perform inference on minibatches of samples for speed. Rather
-    # than taking balanced subsamples, we take a sliding window over a
-    # permutation which is balanced in expectation.
-    perm = _S(R.permutation(n).astype('int32'))
-    epoch = T.iscalar()
-    sample_minibatch = epoch % (n // minibatch_n)
-    index = perm[sample_minibatch * minibatch_n:(sample_minibatch + 1) * minibatch_n]
-    X_s = X[index]
-    y_s = y[index]
+        # We need to perform inference on minibatches of samples for speed. Rather
+        # than taking balanced subsamples, we take a sliding window over a
+        # permutation which is balanced in expectation.
+        perm = _S(R.permutation(n).astype('int32'))
+        epoch = T.iscalar()
+        sample_minibatch = epoch % (n // minibatch_n)
+        index = perm[sample_minibatch * minibatch_n:(sample_minibatch + 1) * minibatch_n]
+        X_s = X[index]
+        y_s = y[index]
 
-    # Variational approximation (re-parameterize eta = X theta). This is a
-    # "Gaussian reconstruction" in that we characterize its expectation and
-    # variance, then approximate its distribution with a Gaussian.
-    #
-    # We need to take the gradient of an intractable integral, so we re-write
-    # it as a Monte Carlo integral which is differentiable, following Kingma &
-    # Welling, ICLR 2014 (http://arxiv.org/abs/1312.6114). When we take the
-    # gradient, the global mean of the reconstruction is constant and drops
-    # out, so we only need to keep the global variance.
-    mu = T.dot(X_s, alpha * beta)
-    nu = T.dot(T.sqr(X_s), alpha / gamma + alpha * (1 - alpha) + T.sqr(beta))
-    random = T.shared_randomstreams.RandomStreams(seed=0)
-    eta_raw = random.normal(size=(10, minibatch_n))
-    eta = mu + T.sqrt(nu) * eta_raw
+        # Variational approximation (re-parameterize eta = X theta). This is a
+        # "Gaussian reconstruction" in that we characterize its expectation and
+        # variance, then approximate its distribution with a Gaussian.
+        #
+        # We need to take the gradient of an intractable integral, so we re-write
+        # it as a Monte Carlo integral which is differentiable, following Kingma &
+        # Welling, ICLR 2014 (http://arxiv.org/abs/1312.6114). When we take the
+        # gradient, the global mean of the reconstruction is constant and drops
+        # out, so we only need to keep the global variance.
+        mu = T.dot(X_s, alpha * beta)
+        nu = T.dot(T.sqr(X_s), alpha / gamma + alpha * (1 - alpha) + T.sqr(beta))
+        random = T.shared_randomstreams.RandomStreams(seed=0)
+        eta_raw = random.normal(size=(10, minibatch_n))
+        eta = mu + T.sqrt(nu) * eta_raw
 
-    # Objective function
-    elbo = (
-        llik(y_s, eta)
-        + .5 * T.sum(alpha * (1 + T.log(tau_deref) - T.log(gamma) - tau_deref * (T.sqr(beta) + 1 / gamma)))
-        - T.sum(alpha * T.log(alpha / pi_deref) + (1 - alpha) * T.log((1 - alpha) / (1 - pi_deref)))
-    )
+        # Objective function
+        elbo = (
+            llik(y_s, eta)
+            + .5 * T.sum(alpha * (1 + T.log(tau_deref) - T.log(gamma) - tau_deref * (T.sqr(beta) + 1 / gamma)))
+            - T.sum(alpha * T.log(alpha / pi_deref) + (1 - alpha) * T.log((1 - alpha) / (1 - pi_deref)))
+        )
 
-    # Maximize ELBO using stochastic gradient descent.
-    #
-    # Adaptive estimation (Kingma & Welling arxiv:1412.6980) tunes the learning
-    # rate based on exponentially weighted moving averages of the first and
-    # second moments of the gradient.
-    if learning_rate is None:
-        learning_rate = 0.5 / n
-    grad = T.grad(elbo, params)
-    M = [_S(_Z(p)) for param in params]
-    V = [_S(_Z(p)) for param in params]
-    a_t = learning_rate * T.sqrt(1 - T.pow(b2, epoch)) / (1 - T.pow(b1, epoch))
-    adam_updates = collections.OrderedDict()
-    for p, g, m, v in zip(params, grad, M, V):
-        new_m = b1 * m + (1 - b1) * g
-        new_v = b2 * v + (1 - b2) * T.sqr(g)
-        adam_updates[p] = T.cast(p + a_t * new_m / (T.sqrt(new_v) + e), _real)
-        adam_updates[m] = new_m
-        adam_updates[v] = new_v
-    vb_step = _F(inputs=[epoch], outputs=elbo, updates=adam_updates)
-    curr_elbo = numpy.array(0)
-    delta = 1
-    t = 0
-    while delta > 0:
-        t += 1
-        new_elbo = vb_step(t)
-        if not t % 1000:
-            delta = new_elbo - curr_elbo
-            curr_elbo = new_elbo
-            yield curr_elbo, alpha.eval(), beta.get_value(), gamma.eval()
+        # Maximize ELBO using stochastic gradient ascent
+        #
+        # Adaptive estimation (Kingma & Welling arxiv:1412.6980) tunes the learning
+        # rate based on exponentially weighted moving averages of the first and
+        # second moments of the gradient.
+        if learning_rate is None:
+            learning_rate = 0.5 / n
+        grad = T.grad(elbo, params)
+        M = [_S(_Z(p)) for param in params]
+        V = [_S(_Z(p)) for param in params]
+        a_t = learning_rate * T.sqrt(1 - T.pow(b2, epoch)) / (1 - T.pow(b1, epoch))
+        adam_updates = collections.OrderedDict()
+        for p, g, m, v in zip(params, grad, M, V):
+            new_m = b1 * m + (1 - b1) * g
+            new_v = b2 * v + (1 - b2) * T.sqr(g)
+            adam_updates[p] = T.cast(p + a_t * new_m / (T.sqrt(new_v) + e), _real)
+            adam_updates[m] = new_m
+            adam_updates[v] = new_v
+        self.vb_step = _F(inputs=[epoch, pi, tau], outputs=elbo,
+                          updates=adam_updates, allow_input_downcast=True)
+
+        # Importance samples for hyperparameters. We draw samples from a
+        # uniform proposal distribution (grid search) so the proposal
+        # probability drops out of the weight.
+        logit_pi_proposal = numpy.linspace(-3, 2, 10).astype(_real)
+        log_tau_proposal = numpy.linspace(-2, 2, 16).astype(_real)
+        logit_pi_prior = scipy.stats.norm()
+        log_tau_prior = scipy.stats.lognorm(s=1)
+        self.pi = _clip(_grid(logit_pi_proposal, logit_pi_proposal))
+        self.tau = _clip(_grid(log_tau_proposal, log_tau_proposal))
+
+    def sgvb(self, task):
+        """Return optimum ELBO and variational parameters which achieve it
+
+        Use Adaptive estimation to tune the learning rate and converge when
+        change in ELBO changes sign. Poll ELBO every thousand iterations to
+        reduce sensitivity to stochasticity.
+
+        """
+        pi = self.pi[task // self.pi.shape[0]]
+        tau = self.tau[task % self.pi.shape[0]]
+        delta = 1
+        t = 1
+        curr_elbo = self.vb_step(t, pi, tau)
+        while delta > 0:
+            t += 1
+            new_elbo = self.vb_step(t, pi, tau)
+            if not t % 1000:
+                delta = new_elbo - curr_elbo
+                if delta > 0:
+                    curr_elbo = new_elbo
+        return curr_elbo, self.alpha.eval(), self.beta.get_value()
+        with open('{}.pkl'.format(task), 'wb') as f:
+            pickle.dump((elbo, alpha, beta), f)
+
+def fit(X, y, a, eps=1e-8, **kwargs):
+    """Return the posterior distributions P(theta, z | x, y, a) and P(pi, tau | x,
+y, a)"""
+    model = Model(X, y, a)
+    with open('work.pkl', 'wb') as f:
+        pickle.dump(model, f)
+    tasks = model.pi.shape[0] * model.tau.shape[0]
+    with drmaa.Session() as s:
+        j = s.createJobTemplate()
+        j.remoteCommand = 'python'
+        j.args = ['-m', 'pybslmm.model']
+        ids = s.runBulkJobs(j, 1, tasks, 1)
+        s.synchronize(ids)
+    log_weights = numpy.zeros(shape=tasks)
+    alpha = numpy.zeros(shape=(tasks, p))
+    beta = numpy.zeros(shape=(tasks, p))
+    for i in range(tasks):
+        with open('{}.pkl'.format(task), 'rb') as f:
+            log_weights[i], alpha[i], beta[i] = pickle.load(f)
+        log_weights[i] += logit_pi_prior.logpdf(scipy.special.logit(pi_)).sum()
+        log_weights[i] += tau_prior.logpdf(tau_).sum()
+    normalized_weights = numpy.exp(log_weights - scipy.misc.logsumexp(log_weights))
+    return [normalized_weights.dot(x) for x in (alpha, beta, pi, tau)]
+
+if __name__ == '__main__':
+    task = int(os.environ['SGE_TASK_ID'])
+    with open('work.pkl', 'rb') as f:
+        model = pickle.load(f)
+    elbo, alpha, beta = model.sgvb(task)
+    with open('{}.pkl'.format(task), 'wb') as f:
+        pickle.dump((elbo, alpha, beta), f)
---
+++ pybslmm/tests/test_model.py
@@ -0,0 +1,26 @@
+import os
+
+import pytest
+
+import pybslmm.model
+import pybslmm.simulation
+
+@pytest.fixture
+def fix_environment():
+    os.environ['LD_LIBRARY_PATH'] = os.getenv('LIBRARY_PATH')
+
+def test_one_task(fix_environment):
+    n=2000
+    p=10000
+    K=.01
+    P=.5
+    pve=0.5
+    seed=0
+    annotation_params = [(100, 1), (50, 1)]
+    with pybslmm.simulation.simulation(p, pve, annotation_params, seed) as s:
+        x, y = s.sample_case_control(n=n, K=K, P=P)
+        x_train, x_test = x[::2], x[1::2]
+        y_train, y_test = y[::2], y[1::2]
+        a = s.annot
+        model = pybslmm.model.Model(x, y, a)
+        elbo, alpha, beta = model.sgvb(100)
