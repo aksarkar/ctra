fc287a7a651be39aaee29350f27ea924430553e4
Abhishek Sarkar <aksarkar@mit.edu>
2016-06-29 01:05:28 -0400

    Fix up environment
    This is needed to work with UGER, which doesn't copy LD_LIBRARY_PATH
    even when requested to execution hosts for security reasons.

--- pybslmm/__init__.py
+++ pybslmm/__init__.py
@@ -1,6 +1,4 @@
-__all__ = [
-    'model',
-    'pcgc',
-    'simulation',
-    'evaluate'
-]
+import os
+
+if 'LD_LIBRARY_PATH' not in os.environ:
+    os.environ['LD_LIBRARY_PATH'] = os.getenv('LIBRARY_PATH')
