7b61a7f298b93bc4a3ab7156c5718959989eca61
Abhishek Sarkar <aksarkar@mit.edu>
2016-09-06 17:32:18 -0400

    Fix test scenarios

--- ctra/evaluate.py
+++ ctra/evaluate.py
@@ -19,63 +19,49 @@ import ctra.pcgc
 import ctra.model
 import ctra.simulation
 
-def prc(y, p):
-    """Precision-recall curve"""
-    for thresh in sorted(p):
-        y_hat = (p > thresh).astype('int')
-        yield sum(y * y_hat) / sum(y + y_hat), sum(y * y_hat) / sum(y)
+def evaluate_gaussian_one_component(n=2000, p=10000, pve=0.5, seed=0):
+    with ctra.simulation.simulation(p, pve, annotation_params, seed) as s:
+        x, y = s.sample_gaussian(n=n)
+        a = numpy.zeros(p, dtype='int8')
+        m = ctra.model.GaussianModel(x, y, a).fit()
+        print(annotation_params, m.pve, m.pi)
 
-def auprc(y, p):
-    """Estimate area under the precision-recall curve using a Riemann sum"""
-    points = numpy.array(sorted(prc(y, p)))
-    delta = numpy.diff(points[:,0], axis=0)
-    return points[1:,1].dot(delta)
-
-def evaluate_gaussian_is(n=2000, p=10000, pve=0.5, seed=0):
+def evaluate_gaussian(n=2000, p=10000, pve=0.5, seed=0):
     annotation_params = [(100, 1), (50, 1)]
     with ctra.simulation.simulation(p, pve, annotation_params, seed) as s:
         x, y = s.sample_gaussian(n=n)
         m = ctra.model.GaussianModel(x, y, s.annot).fit()
         print(annotation_params, m.pve, m.pi)
 
-def evaluate_gaussian_equal_prop(n=2000, p=1000, pve=0.25, seed=0):
+def evaluate_gaussian_equal_prop(n=2000, p=10000, pve=0.5, seed=0):
     annotation_params = [(100, 1), (100, 0.5)]
     with ctra.simulation.simulation(p, pve, annotation_params, seed) as s:
         x, y = s.sample_gaussian(n=n)
         m = ctra.model.GaussianModel(x, y, s.annot).fit()
         print(annotation_params, m.pve, m.pi)
 
-def evaluate_gaussian_binary(n=2000, p=1000, pve=0.25, K=0.01, P=0.5, seed=0):
+def evaluate_gaussian_binary(n=2000, p=10000, pve=0.25, K=0.01, P=0.5, seed=0):
     annotation_params = [(100, 1), (50, 1)]
     with ctra.simulation.simulation(p, pve, annotation_params, seed) as s:
         x, y = s.sample_case_control(n=n, K=K, P=P)
         m = ctra.model.GaussianModel(x, y, s.annot, K=K).fit(atol=1e-2)
         print(annotation_params, m.pve, m.pi)
 
-def evaluate_gaussian_binary_equal_prop(n=2000, p=1000, pve=0.25, K=0.01, P=0.5, seed=0):
+def evaluate_gaussian_binary_equal_prop(n=2000, p=10000, pve=0.25, K=0.01, P=0.5, seed=0):
     annotation_params = [(100, 1), (100, 0.5)]
     with ctra.simulation.simulation(p, pve, annotation_params, seed) as s:
         x, y = s.sample_case_control(n=n, K=K, P=P)
         m = ctra.model.GaussianModel(x, y, s.annot, K=K).fit(atol=1e-2)
         print(annotation_params, m.pve, m.pi)
 
-def test_known_data():
-    with open('genotypes.txt') as f:
-        x = numpy.loadtxt(f, delimiter=',')
-    with open('phenotype.txt') as f:
-        y = numpy.loadtxt(f)
-    w = (y > 0).astype('int')
-    a = numpy.zeros(x.shape[1], dtype='int8')
-    m = ctra.model.GaussianModel(x, y, a).fit()
-    print(m.pi)
-
-def evaluate_sgvb(n=2000, p=1000, K=.01, P=.5, pve=0.5, seed=0):
+def evaluate_sgvb(n=2000, p=10000, K=.01, P=.5, pve=0.5, seed=0):
     annotation_params = [(100, 1), (50, 1)]
     with ctra.simulation.simulation(p, pve, annotation_params, seed) as s:
         x, y = s.sample_case_control(n=n, K=K, P=P)
         a = numpy.zeros(p, dtype='int8')
-        m = ctra.model.LogisticModel(x, y, a, K).fit()
-        print('pi={:.3g}'.format(m.pi))
+        m = ctra.model.LogisticModel(x, y, a, K, learning_rate=1e-4, minibatch_n=1)
+        m.fit(min_iters=10000, poll_iters=10000, weight=.1)
+        print(m.pi)
 
 def evaluate_pcgc_two_components(n=1000, p=1000, pve=0.5):
     """Use PCGC to compute "heritability enrichment" under different architectures
