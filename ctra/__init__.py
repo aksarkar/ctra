5aad56dda6b2f87337bdd31aed04ac3166dfe41c
Abhishek Sarkar <aksarkar@mit.edu>
2017-01-23 21:22:32 -0500

    Remove noisy debug output
    GPy has lots of numerical problems...

--- ctra/__init__.py
+++ ctra/__init__.py
@@ -10,5 +10,3 @@ if 'LD_LIBRARY_PATH' not in os.environ:
     os.environ['LD_LIBRARY_PATH'] = os.getenv('LIBRARY_PATH')
 
 logging.basicConfig(format='[%(asctime)s %(name)s] %(message)s', level=logging.DEBUG)
-numpy.seterrcall(lambda err, flag: logging.warn(err))
-numpy.seterr(all='call')
