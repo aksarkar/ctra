2de065300fcfeefaf03be85be49e16d602fc5554
Abhishek Sarkar <aksarkar@mit.edu>
2017-01-26 01:10:46 -0500

    Move WSABI to separate module

--- ctra/model/base.py
+++ ctra/model/base.py
@@ -14,11 +14,11 @@ import logging
 
 import matplotlib
 import numpy
-import scipy.optimize
-import scipy.spatial
 import scipy.special
 import theano
 import GPy
+
+import ctra.model.wsabi
 
 logger = logging.getLogger(__name__)
 _real = theano.config.floatX
@@ -179,165 +179,6 @@ sampling.
             raise ValueError('Must fit the model before computing Bayes factors')
         return (numpy.log(numpy.exp(self.elbo_vals - self.elbo_vals.max()).mean()) +
                 self.elbo_vals.max())
-
-class _mvuniform():
-    """Multidimensional uniform distribution
-
-    Convenience wrapper to allow sampling m-dimensional vectors using the same
-    API as scipy.stats.multivariate_normal
-
-    """
-    def __init__(self, a, b, m):
-        self.m = m
-        self.uniform = scipy.stats.uniform(loc=a, scale=b - a)
-
-    def rvs(self, size):
-        return self.uniform.rvs(size=self.m * size).reshape(-1, self.m)
-
-    def logpdf(self, x):
-        return self.uniform.logpdf(x).sum()
-
-def _sqdist(a, b, V):
-    """Return (a - b)' V^-1 (a - b) for all pairs (a, b)"""
-    _D = scipy.spatial.distance.cdist
-    return numpy.square(_D(a, numpy.atleast_2d(b),
-                           metric='mahalanobis',
-                           VI=numpy.linalg.pinv(V)))
-
-class WSABI:
-    """Warped sequential approximate Bayesian inference (Gunter et al., NIPS 2016)
-
-    To evaluate the intractable integral \int p(x | theta) p(theta) dtheta, we
-    put a Gaussian process prior to represent uncertainty in p(x | theta) at
-    points theta not yet evaluated. This means we can actively choose the next
-    theta to minimize the uncertainty in the integral (exploration) and
-    maximize the number of high likelihood samples (exploitation).
-
-    """
-    def __init__(self, m, hyperprior=None, proposal=None, max_retries=10):
-        if hyperprior is None:
-            self.hyperprior = scipy.stats.multivariate_normal()
-        else:
-            self.hyperprior = hyperprior
-        if proposal is None:
-            self.proposal = scipy.stats.multivariate_normal()
-        else:
-            self.proposal = proposal
-        self.m = m
-        self.I = numpy.eye(self.m)
-        self.gp = None
-        # ARD kernel overfits/explodes numerically in our problem
-        self.K = GPy.kern.RBF(input_dim=self.m, ARD=False)
-        self.K.lengthscale.constrain_bounded(0, 1)
-        self.x = []
-        self.f = []
-        self._mean = None
-        self._var = None
-        self.max_retries = max_retries
-
-    def __iter__(self):
-        return self
-
-    def _neg_uncertainty(self, query):
-        """Return -V[l(phi) pi(phi)]"""
-        query = numpy.atleast_2d(numpy.squeeze(query))
-        schur_comp = self.gp.rbf.K(query, self.x).dot(self.Kinv)
-        return -(numpy.square(self.hyperprior.pdf(query)) *
-                 (self.gp.rbf.K(query) - schur_comp.dot(self.gp.rbf.K(self.x, query))) *
-                 (self.offset + .5 * schur_comp.dot(self.f)))
-
-    def __next__(self):
-        """Return the next hyperparameter sample phi
-
-        The active sampling scheme finds the point which contributes maximum
-        variance to the current estimate of the model evidence.
-
-        """
-        logger.debug('Actively sampling next point')
-        opt = object()
-        n = 0
-        while n < self.max_retries and not getattr(opt, 'success', False):
-            x0 = self.proposal.rvs(1)
-            logger.debug('Starting minimizer from x0={}'.format(x0))
-            opt = scipy.optimize.minimize(self._neg_uncertainty, x0=x0, method='Nelder-Mead')
-            n += 1
-        if not opt.success:
-            import pdb; pdb.set_trace()
-            raise ValueError('Failed to find next sample')
-        return opt.x
-
-    def transform(self, x, f, exp=True):
-        """Square root transform function values
-
-        Enforce non-negativity in the chi-square process (squared-GP)
-        representing the original function.
-
-        exp - exponentiate log likelihoods
-
-        """
-        f = f[:]
-        if exp:
-            self.log_scaling = f.max()
-            f = numpy.exp(f - self.log_scaling)
-        else:
-            self.log_scaling = None
-        self.offset = .8 * f.min()
-        f = numpy.sqrt(2 * (f - self.offset)).reshape(-1, 1)
-        self.x = x
-        self.f = f
-        return self
-
-    def fit(self):
-        """Fit the GP on the transformed data"""
-        self.gp = GPy.models.GPRegression(self.x, self.f, self.K)
-        self.gp.optimize_restarts()
-        # Pre-compute things
-        A = self.gp.rbf.lengthscale * self.I
-        Ainv = self.I / self.gp.rbf.lengthscale
-        b = self.hyperprior.mean
-        B = self.hyperprior.cov
-        Binv = numpy.linalg.pinv(B)
-        self.Kinv = numpy.linalg.pinv(self.gp.rbf.K(self.x))
-        # The term (K^-1 f)^2 comes outside the integrals for posterior
-        # mean/variance
-        right = numpy.square(self.Kinv.dot(self.f))
-        # \int_X K(x, x_d)^2 N(x; b, B) dx
-        w = (numpy.square(self.gp.rbf.variance) /
-             numpy.sqrt(numpy.linalg.det(2 * Ainv.dot(B) + self.I)) *
-             numpy.exp(-.5 * _sqdist(self.x, b, .5 * A + B)))
-        self._mean = self.log_scaling + numpy.log(self.offset + w.T.dot(right))
-        # \int_X K(x, x_d) N(x; b, B) dx
-        z = (self.gp.rbf.variance /
-             numpy.sqrt(numpy.linalg.det(B)) *
-             numpy.exp(-.5 * _sqdist(self.x, b, A + B)))
-        # The inner integral for the variance of the marginal likelihood:
-        #
-        #    \inv_X K(x, x_d) N(x; b, B) K(x, x') dx
-        #
-        # results in a Gaussian with covariance sigma. The outer integral
-        # results in a Gaussian with covariance _lambda
-        sigma = Ainv + numpy.linalg.pinv(Ainv + Binv)
-        _lambda = numpy.linalg.pinv(sigma) + numpy.linalg.pinv(Ainv + Binv)
-        self._var = (self.gp.rbf.variance * z.T.dot(z) *
-                     numpy.sqrt(numpy.linalg.det(2 * Ainv + Binv) *
-                                numpy.linalg.det(_lambda)) -
-                     w.T.dot(self.Kinv).dot(w))
-        return self
-
-    def mean(self):
-        """Return the posterior mean \int_X m(x) p(x) dx"""
-        if self._mean is None:
-            raise ValueError('Must fit the model before estimating its mean')
-        return numpy.squeeze(self._mean)
-
-    def var(self):
-        """Return the posterior variance \int_X Cov(x, x') p(x) p(x') dx dx' """
-        if self._var is None:
-            raise ValueError('Must fit the model before estimating its variance')
-        return numpy.squeeze(self._var)
-
-    def __repr__(self):
-        return 'WSABI(mean={}, var={})'.format(self.mean(), self.var())
 
 class ActiveSampler(Model):
     """Estimate the posterior p(pi, tau | X, y, A) using WSABI-L (Gunter et al.,
