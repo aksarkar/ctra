869eb3425944cc8011b68bef95001e87d21f77f7
Abhishek Sarkar <aksarkar@mit.edu>
2017-02-14 18:09:55 -0500

    Fix logistic regression bias estimation

--- ctra/model/dsvi.py
+++ ctra/model/dsvi.py
@@ -37,7 +37,7 @@ class DSVI(Algorithm):
 
     """
     def __init__(self, X_, y_, a_, pve, minibatch_n=None, stoch_samples=10,
-                 learning_rate=1e-3, *params, **kwargs):
+                 learning_rate=1e-3, params=None, **kwargs):
         """Compile the Theano function which takes a gradient step"""
         super().__init__(X_, y_, a_, pve)
 
@@ -71,6 +71,8 @@ class DSVI(Algorithm):
         gamma = 1e-3 + T.nnet.softplus(gamma_raw)
 
         self.params = [alpha_raw, beta, gamma_raw]
+        if params is None:
+            params = []
         self.params.extend(params)
 
         # We need to perform inference on minibatches of samples for speed. Rather
@@ -96,31 +98,26 @@ class DSVI(Algorithm):
 
         # Objective function
         if 'elbo' not in self.__dict__:
+            # This is a hack to allow subclasses to modify the graph before we
+            # compile the Theano functions
             self.elbo = 0
+        kl_hyper = -self.elbo
         # The log likelihood is for the minibatch, but we need to scale up
         # to the full dataset size
         error = self._llik(y, eta) * self.scale_n
         # KL(q(theta) || p(theta))
-        kl_theta = .5 * T.sum(alpha * (1 + T.log(tau_deref) - T.log(gamma) - tau_deref * (T.sqr(beta) + 1 / gamma)))
+        kl_theta = .5 * T.sum(alpha * (1 - T.log(tau_deref) + T.log(gamma) + tau_deref * (T.sqr(beta) + 1 / gamma)))
         # KL(q(z) || p(z))
         kl_z = T.sum(alpha * T.log(alpha / pi_deref) + (1 - alpha) * T.log((1 - alpha) / (1 - pi_deref)))
         self.elbo += error - kl_theta - kl_z
 
         logger.debug('Compiling the Theano functions')
-        self._randomize = _F(inputs=[pi, tau], outputs=[],
-                             updates=[(alpha_raw, _Z(p)), (beta, _Z(p)),
-                                      (pi_deref, T.basic.choose(a, pi)),
-                                      (tau_deref, T.basic.choose(a, tau))],
-                             givens=[(a, self.a)])
-        alpha_ = T.vector()
-        beta_ = T.vector()
-        self._initialize = _F(inputs=[alpha_, beta_, pi, tau], outputs=[],
-                              updates=[(alpha_raw, alpha_), (beta, beta_),
-                                       (pi_deref, T.basic.choose(a, pi)),
-                                       (tau_deref, T.basic.choose(a, tau))],
-                              givens=[(a, self.a)],
-                              allow_input_downcast=True)
-
+        init_updates = [(alpha_raw, _Z(p)), (beta, _Z(p)), (gamma_raw, _Z(p)),
+                        (pi_deref, T.basic.choose(a, pi)),
+                        (tau_deref, T.basic.choose(a, tau))]
+        # This is needed to re-initialize logistic regression bias
+        init_updates += [(param, 0) for param in params]
+        self._initialize = _F(inputs=[pi, tau], outputs=[], updates=init_updates, givens=[(a, self.a)])
         grad = T.grad(self.elbo, self.params)
         sgd_updates = [(param, param + learning_rate * g)
                        for param, g in zip(self.params, grad)]
@@ -129,6 +126,8 @@ class DSVI(Algorithm):
                       (y, self.y[sample_minibatch * minibatch_n:(sample_minibatch + 1) * minibatch_n]),
                       (a, self.a)]
         self.vb_step = _F(inputs=[epoch], outputs=self.elbo, updates=sgd_updates, givens=sgd_givens)
+
+        self._trace = _F(inputs=[epoch], outputs=[self.elbo, error, kl_theta, kl_z, kl_hyper, alpha, beta, gamma] + params, givens=sgd_givens)
 
         self._opt = _F(inputs=[], outputs=[alpha, beta])
         logger.debug('Finished initializing')
@@ -149,14 +148,19 @@ class DSVI(Algorithm):
         hyperparams = {'pi': pi, 'tau': tau}
         logger.debug('Starting SGD given {}'.format(hyperparams))
         # Re-initialize, otherwise everything breaks
-        self._randomize(**hyperparams)
+        self._initialize(**hyperparams)
+        self.trace = []
         t = 0
         while t < max_epochs * self.scale_n:
+            self.trace.append(self._trace(t))
+            if not t % 1000:
+                logger.debug('\t'.join('{:.3g}'.format(numpy.asscalar(x)) for x in self.trace[-1][:5]))
             t += 1
             elbo = self.vb_step(epoch=t)
-            if not t % 1000:
-                logger.debug('ELBO\t{}\t{:.6g}'.format(t, numpy.asscalar(elbo)))
-            assert numpy.isfinite(elbo)
+            if not numpy.isfinite(elbo):
+                import pdb; pdb.set_trace()
+            elif elbo >= 0:
+                import pdb; pdb.set_trace()
         return elbo, self._opt()
 
 class GaussianDSVI(DSVI):
@@ -180,8 +184,8 @@ class LogisticDSVI(DSVI):
         self.bias_mean = theano.shared(numpy.array(0, dtype=_real))
         self.bias_log_prec = theano.shared(numpy.array(0, dtype=_real))
         self.bias_prec = 1e-3 + T.nnet.softplus(self.bias_log_prec)
-        # Variational surrogate for the bias term. p(bias) = N(0, 2.5); q(bias) = N(m, v^-1)
-        self.elbo = -.5 * (1 + .4 - T.log(self.bias_prec) - .4 * T.sqr(self.bias_mean))
+        # Variational surrogate for the bias term. p(bias) = N(0, 1); q(bias) = N(m, v^-1)
+        self.elbo = -.5 * (1 + 1 - T.log(self.bias_prec) - T.sqr(self.bias_mean))
         super().__init__(X, y, a, pve, params=[self.bias_mean, self.bias_log_prec], **kwargs)
 
     def _llik(self, y, eta):
