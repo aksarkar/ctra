568931b609782679a44da56264c9ad7949dbb714
Abhishek Sarkar <aksarkar@mit.edu>
2017-05-04 14:45:13 -0400

    Test multiple components

--- ctra/tests/test_sgvb.py
+++ ctra/tests/test_sgvb.py
@@ -31,8 +31,12 @@ def test_gaussian_sgvb_drop():
         x, y = s.sample_gaussian(n=1000)
         m = _fit_model(ctra.model.GaussianSGVB, x, y, s, center_y=True, drop=1)
 
+def test_gaussian_sgvb_two_component():
+    with ctra.simulation.simulation(p=2000, pve=0.5, annotation_params=[(10, 1), (10, 1)], seed=0) as s:
+        x, y = s.sample_gaussian(n=1000)
+        _fit_model(ctra.model.GaussianSGVB, x, y, s, center_y=True)
+
 def test_logistic_sgvb_one_component():
     with ctra.simulation.simulation(p=2000, pve=0.5, annotation_params=[(200, 1)], seed=0) as s:
         x, y = s.sample_ascertained_probit(n=1000, K=0.01, P=0.5)
         _fit_model(ctra.model.LogisticSGVB, x, y, s)
-
