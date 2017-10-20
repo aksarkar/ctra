2c819ef49544036ce4ce5d8818ad84db71d29b24
Abhishek Sarkar <aksarkar@mit.edu>
2017-10-20 11:37:14 -0400

    Fix environment variable hack
    It has to come before import theano, otherwise theano's compiler check
    breaks

--- ctra/__init__.py
+++ ctra/__init__.py
@@ -3,11 +3,14 @@ import os
 
 import numpy
 
+# This is an ugly hack to get around Univa Grid Engine nonsense
+if 'LD_LIBRARY_PATH' not in os.environ and 'LIBRARY_PATH' in os.environ:
+    logging.getLogger('ctra').debug('Setting LD_LIBRARY_PATH={}'.format(os.getenv('LIBRARY_PATH')))
+    os.environ['LD_LIBRARY_PATH'] = os.getenv('LIBRARY_PATH')
+
 import ctra.experiments
 import ctra.model
 import ctra.simulation
 
-if 'LD_LIBRARY_PATH' not in os.environ and 'LIBRARY_PATH' in os.environ:
-    os.environ['LD_LIBRARY_PATH'] = os.getenv('LIBRARY_PATH')
-
 logging.basicConfig(format='[%(asctime)s %(name)s] %(message)s', level=logging.DEBUG)
+
