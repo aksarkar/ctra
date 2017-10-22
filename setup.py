3bb479e0ecd97b8d58d39ee9354fb5733f36b576
Abhishek Sarkar <aksarkar@test3.irc.sara.nl.irc.sara.nl>
2017-10-22 22:20:52 +0200

    Add missing dependency

--- setup.py
+++ setup.py
@@ -8,12 +8,22 @@ setuptools.setup(
     author='Abhishek Sarkar',
     author_email='aksarkar@mit.edu',
     license='MIT',
-    install_requires=['h5py', 'matplotlib', 'numpy', 'pandas', 'scipy', 'sklearn', 'theano'],
+    install_requires=[
+        'h5py',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'pyplink',
+        'scipy',
+        'sklearn',
+        'theano',
+    ],
     entry_points={
         'console_scripts': [
             'ctra-evaluate=ctra.evaluate:evaluate',
             'ctra-fit=ctra.fit:main',
             'ctra-convert=ctra.convert:convert'
         ]
-    }
+    },
+    packages=setuptools.find_packages(),
 )
